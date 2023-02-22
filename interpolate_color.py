52dc6c790cafaf00613796894ce9d9cd0f18b2a2
Christos <giachoud@gapps.auth.gr>
2023-02-22 19:38:04 +0200

    Interpolation fixxed

--- interpolate_color.py
+++ interpolate_color.py
@@ -1,15 +1,29 @@
 import numpy as np
+import math
 
-
-def interpolate_color(x1, x2, x, c1, c2):
-    diff = abs(x2 - x1)
+def interpolate_color(x1, x2, x, c1: np.array, c2: np.array):
+    """
+    ___3D point RGB color interpolation function___
+    x1: first 3D point
+    x2: second 3D point
+    x: point at which we will compute the interpolation
+    c1: x1's RGB color
+    c2: x2's RGB color
+    """
+    diff1 = abs(x2[0] - x1[0])
+    diff2 = abs(x2[1] - x1[1])
+    print("diff1")
+    print(diff1)
+    print("diff2")
+    print(diff2)
     # if the two points are the same(x1 == x2)
-    if diff == 0:
+    if (diff1 == 0) and (diff2 == 0):
         value = 0.5 * c1 + 0.5 * c2
     # if the 2 points are different
     else:
-        # normalized position of x between x1 and x2
-        norm_pos = abs((x-x1)/(x2-x1))
+        d1 = math.sqrt((x1[0]-x[0])**2 + (x1[1]-x[1])**2)
+        d2 = math.sqrt((x2[0]-x[0])**2 + (x2[1]-x[1])**2)
+        norm_pos = d1/(d1+d2)
         # the result from the interpolation
         value = norm_pos * c1 + (1-norm_pos) * c2
     return value
