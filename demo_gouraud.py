88e808e74db5004fa8564874db061d401f9d4903
Christos <giachoud@gapps.auth.gr>
2023-04-25 16:09:31 +0300

    Rotation and new file

--- demo_gouraud.py
+++ demo_gouraud.py
@@ -1,9 +1,10 @@
 import numpy as np
 import render
 import matplotlib.pyplot
+import cv2
 
 # load the data of the image
-data = np.load('hw1.npy', allow_pickle=True)
+data = np.load('h1.npy', allow_pickle=True)
 verts2d = data[()]['verts2d']
 vcolors = data[()]['vcolors']
 faces = data[()]['faces']
@@ -11,6 +12,7 @@ depth = data[()]['depth']
 
 # render the image
 image = render.render(verts2d, faces, vcolors, depth, 'gouraud')
+image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 matplotlib.pyplot.imshow(image)
 matplotlib.pyplot.savefig('gouraud.png')
 matplotlib.pyplot.show()
